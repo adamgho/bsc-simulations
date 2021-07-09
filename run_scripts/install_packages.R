cc3d4fc9586e5a8f71143d1a7fc764bba0d96372
adamgho <nrm545@alumni.ku.dk>
2021-07-09 20:06:36 +0200

    Added README again and install_packages.R

---
+++ run_scripts/install_packages.R
@@ -0,0 +1,8 @@
+# Installs the required R packages
+
+install.packages("MASS")
+install.packages("Matrix")
+install.packages("tidyverse")
+install.packages("igraph")
+install.packages("InvariantCausalPrediction")
+
