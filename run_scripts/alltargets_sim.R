4d52cde4521c8988312b35df9f97b612f24e9707
adamgorm <nrm545@alumni.ku.dk>
2021-07-11 18:12:14 +0200

    Minor fixes

--- run_scripts/alltargets_sim.R
+++ run_scripts/alltargets_sim.R
@@ -1,13 +1,17 @@
 #!/usr/bin/env Rscript
 
+# Command line arguments
 params <- as.numeric(commandArgs(trailingOnly = TRUE))
+
+# Indices of DAGs to simulate from
+DAGs_to_sim <- 1:10
 
 # Only loads libraries and runs if the directory doesn't already exist.
 if (!dir.exists(sprintf("data/alltargets_%d_%d_sdw%d_sdh%d",
                         params[1], params[2], params[3], params[4])))
 {
     source("tools/data_tools.R")
-    DAG_list <- readRDS("data/DAGs1000_nx30_nh30_probconnect04.rds")
+    DAG_list <- readRDS("data/DAGs1000_nx30_nh30_probconnect04.rds")[DAGs_to_sim]
     sim_alltargets_datasets(
         DAG_list, params[1], params[2], params[3], params[4],
         dir = "data/", max_MB_per_file = 500
--- run_scripts/singletargets_sim.R
+++ run_scripts/singletargets_sim.R
@@ -1,6 +1,10 @@
 #!/usr/bin/env Rscript
 
+# Command line arguments
 params <- as.numeric(commandArgs(trailingOnly = TRUE))
+
+# Indices of the DAGs to simulate from
+DAGs_to_sim <- 1:10
 
 # Only loads libraries and runs if the directory doesn't already exist.
 if (!dir.exists(sprintf("data/singletargets_%d_%d_%d_%d_sdw%d_sdh%d",
@@ -8,7 +12,7 @@ if (!dir.exists(sprintf("data/singletargets_%d_%d_%d_%d_sdw%d_sdh%d",
                         params[4], params[5], params[6])))
 {
     source("tools/data_tools.R")
-    DAG_list <- readRDS("data/DAGs1000_nx30_nh30_probconnect04.rds")
+    DAG_list <- readRDS("data/DAGs1000_nx30_nh30_probconnect04.rds")[DAGs_to_sim]
     sim_singletargets_datasets(
         DAG_list,
         params[1], params[2], params[3], params[4], params[5], params[6],
