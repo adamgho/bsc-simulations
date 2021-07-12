b5b008b128af24005c92754397aeb7c7cf39e2a9
adamgorm <nrm545@alumni.ku.dk>
2021-07-12 20:37:17 +0200

    Added optional DAG filename

--- run_scripts/singletargets_sim.R
+++ run_scripts/singletargets_sim.R
@@ -5,14 +5,14 @@ params <- as.numeric(commandArgs(trailingOnly = TRUE))
 
 # Indices of the DAGs to simulate from
 DAGs_to_sim <- 1:10
-
+DAGs_filename <- "data/DAGs1000_nx30_nh30_probconnect04.rds" 
 # Only loads libraries and runs if the directory doesn't already exist.
 if (!dir.exists(sprintf("data/singletargets_%d_%d_%d_%d_sdw%d_sdh%d",
                         params[1], params[2], params[3],
                         params[4], params[5], params[6])))
 {
     source("tools/data_tools.R")
-    DAG_list <- readRDS("data/DAGs1000_nx30_nh30_probconnect04.rds")[DAGs_to_sim]
+    DAG_list <- readRDS(DAGs_filename)[DAGs_to_sim]
     sim_singletargets_datasets(
         DAG_list,
         params[1], params[2], params[3], params[4], params[5], params[6],
