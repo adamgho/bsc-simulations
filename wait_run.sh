82d1f48dd5d9650b46a4e36fc44baac5a65d7a3c
adamgorm <public@adamgorm.dk>
2021-07-28 14:43:35 +0200

    Fixed wait_run.sh

--- wait_run.sh
+++ wait_run.sh
@@ -7,7 +7,7 @@ while ! $STARTED; do
 	echo $NR
 	if [ $NR -eq 0 ]; then
 		echo 'data/sep_nx5_local/' > run_scripts/data_dir.txt
-		echo 'data/separate_data_tools.R' > run_scripts/data_tools_file.txt
+		echo 'tools/separate_data_tools.R' > run_scripts/data_tools_file.txt
 		sh ./run_scripts/run_sim_and_tpr_fpr_and_AUC.sh -p 4
 		STARTED=true
 	fi
