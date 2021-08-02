9b53696b286f49dc567354854535e0d8254f4159
adamgorm <public@adamgorm.dk>
2021-08-02 15:40:38 +0200

    Updated some of README and cleaned up a few files

--- run_scripts/singletargets_tpr_fpr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Generates tpr_fpr files.
-
-Rscript run_scripts/singletargets_OLS.R
-Rscript run_scripts/singletargets_POLS.R
-Rscript run_scripts/singletargets_DPOLS.R
-# Rscript run_scripts/singletargets_ICP.R
