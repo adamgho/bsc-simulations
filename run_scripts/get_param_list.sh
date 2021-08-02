23f3cb547249a610e5c33be62acc8c95a166a2a3
adamgorm <public@adamgorm.dk>
2021-08-02 18:52:18 +0200

    Added parameters used in thesis

--- run_scripts/get_param_list.sh
+++ run_scripts/get_param_list.sh
@@ -10,8 +10,8 @@ else
 	DIR=$1
 fi
 
-## Saves in txt files in current dir.
+## Saves in txt files in current directory
 
-ls $DIR | grep "^singletargets" | sed 's/singletargets_\([0-9]\+\)_\([0-9]\+\)_\([0-9]\+\)_\([0-9]\+\)_sdw\([0-9]\+\)_sdh\([0-9]\+\)/\1 \2 \3 \4 \5 \6/' > "singletargets_params_${DIR}.txt"
+ls $DIR | grep "^singletargets" | sed 's/singletargets_\([0-9]\+\)_\([0-9]\+\)_\([0-9]\+\)_\([0-9]\+\)_sdw\([0-9]\+\)_sdh\([0-9]\+\)/\1 \2 \3 \4 \5 \6/' > "alltargets_params_auto.txt"
 
-ls $DIR | grep "^alltargets" | sed 's/alltargets_\([0-9]\+\)_\([0-9]\+\)_sdw\([0-9]\+\)_sdh\([0-9]\+\)/\1 \2 \3 \4/' > "alltargets_params_${DIR}.txt"
+ls $DIR | grep "^alltargets" | sed 's/alltargets_\([0-9]\+\)_\([0-9]\+\)_sdw\([0-9]\+\)_sdh\([0-9]\+\)/\1 \2 \3 \4/' > "singletargets_params_auto.txt"
---
+++ thesis_parameters/nx30nh30/README.md
@@ -0,0 +1,7 @@
+# Simulations with 30 X and 30 H
+
+I ran these on 1000 DAGs with 30 X and 30 H, except for the
+simulations in stor 4, were only 100 DAGs where used.
+
+Don't run ICP and PICP for these setting. They are very slow, and all the
+p-values become 1 due to Bonferroni correction for the large number of variables.
